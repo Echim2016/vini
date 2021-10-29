fbaad7db9852fdc882088c97823371af26a29f81
Yi-Chin Hsu <ychim26@gmail.com>
2021-10-29 11:10:36 +0800

    Setup blank page of  Mailbox and Achievement

---
+++ vini/Controller/Achievement/AchievementViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AchievementViewController.swift
+//  vini
+//
+//  Created by Yi-Chin Hsu on 2021/10/29.
+//
+
+import UIKit
+
+class AchievementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ vini/Controller/Mailbox/MailboxViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MailboxViewController.swift
+//  vini
+//
+//  Created by Yi-Chin Hsu on 2021/10/28.
+//
+
+import UIKit
+
+class MailboxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
